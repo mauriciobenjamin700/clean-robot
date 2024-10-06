ab008637a013d2ddaabb7052e78d746d90ed1a54
Mauricio Benjamin <mauriciobenjamin700@gmail.com>
2024-10-06 17:20:19 -0300

    quase la

--- test.py
+++ test.py
@@ -27,9 +27,14 @@ def show_board(board: list[list[int]]):
     """
     print the board
     """
-    for line in board:
-        for column in line:
-            print(column, end=" ")
+    print("-"*30)
+    coluns, lines = get_board_sizes(board)
+
+    for line in range(lines):
+        for column in range(coluns):
+            print(board[line][column], end=" ")
+
+        print("\n")
 
 def get_board_sizes(board: list[list[int]]) -> tuple[int,int]:
     """
@@ -63,6 +68,7 @@ def new_robot_position(robot: tuple[int,int], direction: str) -> tuple[int,int]:
 
     return (x + dx, y + dy)
 
+
 def in_board(board: list[list[int]], position: tuple[int,int]) -> bool:
     """
     return True if the position is in the board
@@ -76,23 +82,27 @@ def is_valid_position(board: list[list[int]], position: tuple[int,int]) -> bool:
     """
     return True if the position is valid
     """
+
+    if not position:
+        return False
     
     if in_board(board, position):
         x, y = position
-        return board[x][y] != OBSTACLE and board[x][y] != ROBOT
-    
+        if board[y][x] != OBSTACLE and board[y][x] != ROBOT:
+            return True
     return False
 
-def generate_moves(board: list[list[int]], robot: tuple[int,int]) -> list[tuple[int,int]]:
+def generate_moves(board: list[list[int]]) -> list[tuple[int,int]]:
     """
     return a list of valid moves [up, down, left, right] in tuple format (x, y)
     """
     global directions
 
+    robot = get_robot(board)
+
     moves = []
 
     for name in directions.keys():
-        print(name)
         new_position = new_robot_position(robot, name)
 
         if is_valid_position(board, new_position):
@@ -105,28 +115,31 @@ def stack_movies(stack:list, movies: list[tuple[int]]) -> list:
     add movies to the stack
     """
     movies.reverse()
+    global visited
+
     for move in movies:
-        if move not in stack:
+        if move not in stack and (not move in visited):
             stack.insert(0, move)
 
     return stack
 
-def get_next_move(stack: list) -> tuple[int, int]:
+def get_next_move(stack: list) -> tuple[int, int] | None:
     """
     return the next move
     """
-    return stack.pop(0)
+    move = None
+    if len(stack) > 0:
+        move = stack.pop(0)
+    return move
 
 def can_move(board: list[list[int]], position: tuple[int,int]) -> bool:
     """
     return True if the robot can move
     """
 
-    robot = get_robot(board)
-
     if is_valid_position(board, position):
 
-        valid_positions = generate_moves(board, robot)
+        valid_positions = generate_moves(board)
         for option in valid_positions:
             if position == option:
                 return True
@@ -140,9 +153,10 @@ def bfs(board: list[list[int]], start: tuple[int, int], goal: tuple[int, int]):
     """
 
     rows, cols = len(board), len(board[0])
-    directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # cima, baixo, esquerda, direita
+    
+    valid_directions = [directions["up"], directions["down"], directions["left"], directions["right"]]  # cima, baixo, esquerda, direita
     queue = deque([(start, [start])])  # fila de (posição atual, caminho até aqui)
-    visited = set([start])
+    local_visited = set([start])
 
     while queue:
         current, path = queue.popleft()
@@ -150,13 +164,13 @@ def bfs(board: list[list[int]], start: tuple[int, int], goal: tuple[int, int]):
             path.pop(0)
             return path
 
-        for direction in directions:
+        for direction in valid_directions:
 
-            next_row, next_col = current[0] + direction[0], current[1] + direction[1]
+            next_row, next_col = current[1] + direction[1], current[0] + direction[0]
 
-            if 0 <= next_row < rows and 0 <= next_col < cols and (next_row, next_col) not in visited:
+            if 0 <= next_row < rows and 0 <= next_col < cols and (next_row, next_col) not in local_visited:
 
-                visited.add((next_row, next_col))
+                local_visited.add((next_row, next_col))
 
                 queue.append(((next_row, next_col), path + [(next_row, next_col)]))
 
@@ -166,24 +180,29 @@ def move_robot(board: list[list[int]], new_position: tuple[int,int] ) -> list[li
     """
     move the robot to the new position
     """
-    x, y = new_position
-    robot_x, robot_y = get_robot(board)
 
     if is_valid_position(board, new_position):
+        global visited
+
+        x, y = new_position
+        robot_x, robot_y = get_robot(board)
 
         if can_move(board, new_position):
-            board[robot_x][robot_y] = CLEAN
-            board[x][y] = ROBOT
-            print(show_board(board))
+            board[robot_y][robot_x] = CLEAN
+            board[y][x] = ROBOT
+            show_board(board)
         else:
             path = bfs(board, (robot_x, robot_y), new_position)
 
             for position in path:
+                robot_x, robot_y = get_robot(board)
                 x, y = position
-                board[robot_x][robot_y] = CLEAN
-                board[x][y] = ROBOT
-                robot_x, robot_y = x, y
-                print(show_board(board))
+                board[robot_y][robot_x] = CLEAN
+                board[y][x] = ROBOT
+                show_board(board)
+
+        visited.add(new_position)
+
                     
 
     return board
@@ -192,8 +211,7 @@ def move_robot(board: list[list[int]], new_position: tuple[int,int] ) -> list[li
 stack = [get_robot(board)]
 
 while len(stack) > 0:
-    robot = stack.pop(0)
-    moves = generate_moves(board, robot)
+    next = get_next_move(stack)
+    moves = generate_moves(board)
     stack = stack_movies(stack, moves)
-    move_robot(board, get_next_move(stack))
-
+    board = move_robot(board, next)
